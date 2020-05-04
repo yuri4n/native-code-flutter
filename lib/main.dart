cb4f0f09262d7d00eeda1b98b1dd33a507530b9b
Julian Garzon <bryangarzon22@gmail.com>
2020-05-04 00:09:28 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel;
+
+  Future<void> _getBatteryLevel() async {
+    const platform = MethodChannel('course.flutter.dev/battery');
+    try {
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    } on PlatformException catch (error) {
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BatteryLevel'),
+      ),
+      body: Center(
+        child: Text(
+          'Battery Level: $_batteryLevel',
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ),
+    );
+  }
+}
